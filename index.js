aa6f7ce81462d994ba317f24b6eb4794fd773db2
afeuerei14 <85976405+afeuerei14@users.noreply.github.com>
2021-08-26 21:19:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ const fs = require("fs");
 const util = require("util");
 const generateReadme = require("./utils/generateReadme");
 const writeFileAsync = util.promisify(fs.writeFile);
-// const generateReadme = require("./utils/generateReadme")
-// const writeFileAsync = util.promisify(fs.writeFile);
-// const util = require("util");
-// const generateReadme = require("./utils/generateReadme")
-// const writeFileAsync = util.promisify(fs.writeFile);
-
-// TODO: Create an array of questions for user input
-//thought this could work but it looks like it"s not going to
-// function Readme(description, contents, installation, usage, license, contributing, tests, questions) {
-//     this.title = title;
-//     this.description = description;
-//     this.contents = contents;
-//     this.installation = installation;
-//     this.usage = usage
-
-//     this.license = license;
-//     this.contributing = contributing;
-//     this.tests = tests;
-//     this.questions = questions;
-// }
 
 const promptUser = () => {
 return inquirer.prompt([
@@ -130,42 +110,4 @@ return inquirer.prompt([
       }
       
       init();  
-    // function init () {
-    //     // try{
-    //     // const readme = generateReadme(answers);
-    //     // const init = () => {
-    //     // const answers = await promptUser();
-    //     const generateContent = generateReadme(answers);
-    //     // Write new README.md to dist directory
-    //     await writeFileAsync("./dist/README.md", generateContent);
-    // }
-    //     //     promptUser()
-    // //       .then((answers) => fs.writeFileAsync("./utils/README,md", generateReadme(answers)))
-    // //       .then(() => console.log("Successfully wrote to index.html"))
-    // //       .catch((err) => console.error(err));
-    // //   };
-    //   init();
-    
-    // async function init() {
-    //     try {
-    //         // Ask user questions and generate responses
-    //         const answers = await promptUser();
-    //         const generateContent = generateReadme(answers);
-    //         // Write new README.md to dist directory
-    //         await fs.appendFile("./utils/README.md", generateContent);
-    //         console.log("✔️  Successfully wrote to README.md");
-    //     }   catch(err) {
-    //         console.log(err);
-    //     }
-    //   }
-      
-    //   init();  
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-// await writeFileAsync("./dist/README.md", generateContent);
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+   
